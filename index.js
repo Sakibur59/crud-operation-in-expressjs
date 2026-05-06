59d6d19673ce443f7f12803ae8adaf72d53e5c56
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-06 22:23:23 +0600

    display user details function added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -24,23 +24,29 @@ app.get("/", (req, res) => {
 
 const run = async () => {
   try {
-    
     await client.connect();
-    const db = client.db('simpleCrud');
-    const userCollection= db.collection('users')
+    const db = client.db("simpleCrud");
+    const userCollection = db.collection("users");
 
-    app.get('/users',async(req,res)=>{
-        const cursor = userCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    
     // await client.close();
   }
 };
