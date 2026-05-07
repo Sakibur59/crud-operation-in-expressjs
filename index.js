7783ae3c1c085ffc18ed648c9552f77d0e9ddffa
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-07 12:24:07 +0600

    create new user post method

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ const run = async () => {
       const user = await userCollection.findOne(query);
       res.send(user);
     });
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = {
